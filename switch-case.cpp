d10941e02db9264993a1917c600d2f53cd03497e
Plexus Network Team <turkey.plexus.system@gmail.com>
2022-09-23 11:49:24 +0300

    Create switch-case.cpp

---
+++ switch-case.cpp
@@ -0,0 +1,13 @@
+int number;
+cout << "Bir sayı gir: "
+cin >> number;
+switch(number) {
+case 1:
+    cout << "1 değeri girildi." << endl;
+    break;
+case 2:
+    cout << "2 değeri girildi." << endl;
+    break;
+deault:
+    cout << "Girilen değer: " << number << endl;
+}
